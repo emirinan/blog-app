026d18aa6152d0859b9dbd186103ec91c99a2d50
Emir Inan <69512940+emirinan@users.noreply.github.com>
2025-04-23 12:19:20 +0300

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -29,7 +29,7 @@ export default async function DashboardRoute() {
       <div className="flex items-center justify-between mb-4">
         <h2 className="text-xl font-medium">Your Blog Articles</h2>
 
-        <Link className={buttonVariants({ variant: "outline" }} href="/dashboard/create">
+        <Link className={buttonVariants({ variant: "outline" })} href="/dashboard/create">
           Create Post
         </Link>
       </div>
